ce316a501f39a5ded79a32cd3be02d77962b01c5
Beyonder22 <ak6401790@gmail.com>
2023-10-07 13:41:20 +0530

    first commit

---
+++ Post.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const PostSchema = new mongoose.Schema({
+	title: String,
+	summary: String,
+	content: String,
+	cover: String,
+	author:{type:mongoose.Schema.Types.ObjectId, ref:'User'},
+},{
+	timestamps: true,
+});
+
+export default mongoose.model('Post', PostSchema);
